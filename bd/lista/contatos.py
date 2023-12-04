dd773bfc1ffa5f7db9ef3b074edc2d7c52243b73
abeazist <beatrizoliveirasantos646@gmail.com>
2023-12-04 20:25:57 -0300

    populando a tabela

--- bd/lista/contatos.py
+++ bd/lista/contatos.py
@@ -15,8 +15,7 @@ def executa_comando(conn, comando: str): #nsei praq serve ainda
     cur = conn.cursor()
     # executa um statement
     cur.execute(comando)
-    db_version = cur.fetchone()
-    print(db_version)
+    
     cur.close()
 
 ''' 
@@ -24,18 +23,48 @@ def executa_comando(conn, comando: str): #nsei praq serve ainda
 
 def criar_tabela_aluno():
     conn= get_conexao_postgres("contatos", "postgres", "postgres")
+    #criando tabela contatos
     cursor=conn.cursor()
     cursor.execute("""
         CREATE TABLE Contatos(
-                   RA INTEGER PRIMARY KEY,
+                   ID INTEGER PRIMARY KEY,
                    nome VARCHAR (100) NOT NULL,
+                   DataNascimento DATE,
+                   Telefone VARCHAR(20),
+                   Rua VARCHAR(255),
+                   NumeroResidencia VARCHAR(10),
+                   Complemento VARCHAR(255),
+                   Cidade VARCHAR(255),
+                   Estado VARCHAR(50),
+                   Pais VARCHAR(50),
+                   CEP VARCHAR(10)
+        );
+    """)
+    ID = 737289930
+    nome = 'Beatriz'
+    Datanascimento = date(2006, 7, 22)
+    Telefone = '44 997200430'
+    Rua = 'Ovidio da miami'
+    NumeroResidencia = '254'
+    Complemento = 'casa'
+    Cidade = "Engenheiro Beltrão"
+    Estado = 'Paraná'
+    Pais = 'Brasil'
+    CEP = '8727000'
+
+    cursor.execute("""
+            INSERT INTO Contatos (ID,nome,Datanascimento,Telefone,Rua,NumeroResidencia,Complemento,Cidade,Estado,Pais,CEP)
+            VALUES(%s, %s, %s, %s,%s, %s, %s, %s,%s, %s, %s);
+                   """,(ID,nome,Datanascimento,Telefone,Rua,NumeroResidencia,Complemento,Cidade,Estado,Pais,CEP))
+
+    #INSERIR REGISTRO NA TABELA
+
+    Contatos = [
+        (635626793,'Fernanda',date(2007,7,22))
+    ]
                    
-        )
                    
-                   
-                   
-"""
-                   )
+                
 
 def main(): #valor na funcao de conexao
     conn = get_conexao_postgres("contatos", "postgres", "postgres")
